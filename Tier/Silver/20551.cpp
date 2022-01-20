7dd6c34c4423d4c4f9c0700fb79e8473897faf5b
HyeonSeong <xkdtndua15@gmail.com>
2022-01-20 16:13:34 +0900

    20551

---
+++ Tier/Silver/20551.cpp
@@ -0,0 +1,113 @@
+
+#include <iostream>
+
+int Copy[1000000];
+int List[1000000];
+
+void Merge(int list[], int left, int mid, int right);
+void MergeSort(int list[], int left, int right);
+
+int Search(int list[], int left, int right, int _Value)
+{
+    int mid = 0;
+    int Output = -1;
+
+    while (left <= right)
+    {
+        mid = (left + right) / 2;
+
+        if (list[mid] >= _Value)
+        {
+            Output = mid;
+            right = mid - 1;
+        }
+
+        else
+            left = mid + 1;
+    }
+
+    if (list[Output] != _Value)
+        Output = -1;
+
+    return Output;
+}
+
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int N, M;
+
+    cin >> N >> M;
+
+    for (int i = 0; i < N; i++)
+    {
+        cin >> List[i];
+    }
+
+    MergeSort(List, 0, N - 1);
+
+    for (int i = 0; i < M; i++)
+    {
+        int Out;
+        cin >> Out;
+
+        int Output = Search(List, 0, N - 1, Out);
+
+        cout << Output;
+
+        if (i < M - 1)
+            cout << "\n";
+    }
+
+    return 0;
+}
+
+void Merge(int list[], int left, int mid, int right)
+{
+    int i, j, k;
+    i = left;
+    j = mid + 1;
+    k = left;
+
+    while (i <= mid && j <= right)
+    {
+        if (list[i] < list[j])
+            Copy[k++] = list[i++];
+        else
+            Copy[k++] = list[j++];
+    }
+
+    if (i > mid)
+    {
+        for (; j <= right; j++)
+            Copy[k++] = list[j];
+    }
+
+    else
+    {
+        for (; i <= mid; i++)
+            Copy[k++] = list[i];
+    }
+
+    for (int l = left; l <= right; l++)
+    {
+        list[l] = Copy[l];
+    }
+}
+
+void MergeSort(int list[], int left, int right)
+{
+    int mid = 0;
+
+    if (left < right)
+    {
+        mid = (left + right) / 2;
+
+        MergeSort(list, left, mid);
+        MergeSort(list, mid + 1, right);
+        Merge(list, left, mid, right);
+    }
+}
