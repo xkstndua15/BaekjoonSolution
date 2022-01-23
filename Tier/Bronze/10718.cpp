f23c47374f731716c2610e082e4d4e59a4edf281
HyeonSeong <xkdtndua15@gmail.com>
2022-01-23 23:36:29 +0900

    10718

---
+++ Tier/Bronze/10718.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cout << "강한친구 대한육군\n";
+    cout << "강한친구 대한육군";
+
+    return 0;
+}
