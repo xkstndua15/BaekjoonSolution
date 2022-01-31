50b45ecb224393309854aa5f3d3af8f9ad0783af
xkstndua15 <xkdtndua15@gmail.com>
2022-01-31 19:37:30 +0900

    2446

---
+++ Tier/Bronze/2446.cpp
@@ -0,0 +1,60 @@
+
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N;
+
+    cin >> N;
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            cout << " ";
+        }
+
+        for (int j = N - i - 1; j > 0; j--)
+        {
+            cout << "*";
+        }
+
+        cout << "*";
+
+        for (int j = N - i - 1; j > 0; j--)
+        {
+            cout << "*";
+        }
+
+        cout << "\n";
+    }
+
+    for (int i = N - 2; i >= 0; i--)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            cout << " ";
+        }
+
+        for (int j = N - i - 1; j > 0; j--)
+        {
+            cout << "*";
+        }
+
+        cout << "*";
+
+        for (int j = N - i - 1; j > 0; j--)
+        {
+            cout << "*";
+        }
+
+        cout << "\n";
+    }
+
+    return 0;
+}
