f8fd29422ad4616dc4ffd026175ab47d3101140d
xkdtndua15@gmail.com <xkdtndua15@gmail.com>
2022-02-13 17:53:11 +0900

    2444

---
+++ Tier/Bronze/2444.cpp
@@ -0,0 +1,52 @@
+
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	int N;
+
+	cin >> N;
+
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N - i - 1; j++)
+		{
+			cout << " ";
+		}
+
+		for (int j = N - i; j < N; j++)
+		{
+			cout << "*";
+		}
+
+		for (int j = N - i; j < N; j++)
+		{
+			cout << "*";
+		}
+
+		cout << "*\n";
+	}
+
+	for (int i = 1; i < N; i++)
+	{
+		for (int j = N - i; j < N; j++)
+		{
+			cout << " ";
+		}
+
+		for (int j = 0; j < N - i - 1; j++)
+		{
+			cout << "*";
+		}
+
+		for (int j = 0; j < N - i - 1; j++)
+		{
+			cout << "*";
+		}
+
+		cout << "*\n";
+	}
+
+	return 0;
+}
